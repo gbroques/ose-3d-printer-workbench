c5fd553ff3e2371a6642caaab4c31a18ef176bbb
G Roques <groques360@gmail.com>
2020-02-02 21:06:54 -0600

    Add more information to command_registry package docstring

--- ose3dprinter_workbench/command_registry/__init__.py
+++ ose3dprinter_workbench/command_registry/__init__.py
@@ -1,4 +1,7 @@
-"""Base class for collections of commands like toolbars, menus, and context menu."""
+"""
+Contains command registry,
+and base class for collections of commands like toolbars.
+"""
 from importlib import import_module
 
 from .command_collection import CommandCollection
