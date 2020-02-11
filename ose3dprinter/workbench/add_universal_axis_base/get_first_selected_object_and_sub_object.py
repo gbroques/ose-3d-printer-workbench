8011c35acb8957c57c9dea9d35124d4a02a06c95
G Roques <groques360@gmail.com>
2020-02-10 19:06:48 -0600

    Improve docstring for get_first_selected_object_and_sub_object

--- ose3dprinter/workbench/add_universal_axis_base/get_first_selected_object_and_sub_object.py
+++ ose3dprinter/workbench/add_universal_axis_base/get_first_selected_object_and_sub_object.py
@@ -1,25 +1,26 @@
 from ose3dprinter.core.exceptions import AxisFrameAttachmentError
 
 
-def get_first_selected_object_and_sub_object(selection):
+def get_first_selected_object_and_sub_object(selection_objects):
     """Gets first selected object and sub-object.
 
-    :param selection: Selection object from Gui.Selection.getSelectionEx()
-    :type selection: Gui::SelectionObject
+    :param selection_objects: List of selection objects;
+                              from Gui.Selection.getSelectionEx()
+    :type selection_objects: List of Gui::SelectionObject
     :raises AxisFrameAttachmentError: If there's not a single selected object,
                                       and not a single sub-object.
-    :return: First selected object, and first sub-object
+    :return: First selected object and first sub-object
     :rtype: Two-element tuple containing object and sub-object
     """
-    num_selected = len(selection)
+    num_selected = len(selection_objects)
     if num_selected != 1:
         raise AxisFrameAttachmentError(
             'Selected {} instead of 1 element'.format(num_selected))
-    first_selection = selection[0]
-    num_sub_objects = len(first_selection.SubObjects)
+    first_selection_object = selection_objects[0]
+    num_sub_objects = len(first_selection_object.SubObjects)
     if num_sub_objects != 1:
         message_template = 'Selected object has {} sub objects instead of 1'
         raise AxisFrameAttachmentError(
             message_template.format(num_sub_objects))
 
-    return first_selection.Object, first_selection.SubObjects[0]
+    return first_selection_object.Object, first_selection_object.SubObjects[0]
