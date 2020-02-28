d52cd2ffb55c2ae02ef145366da956e8cb7fccb8
G Roques <groques360@gmail.com>
2020-02-27 19:03:38 -0600

    Fix heated bed placement when frame is moved and selected (Closes #6)

--- ose3dprinter/core/get_heated_bed_frame_centered_kwargs.py
+++ ose3dprinter/core/get_heated_bed_frame_centered_kwargs.py
@@ -2,10 +2,10 @@ from FreeCAD import Placement, Vector, Rotation
 
 
 def get_heated_bed_frame_centered_kwargs(frame):
-    half_frame_size = frame.Size / 2
-    z = frame.Placement.Base.z
-    placement = Placement(
-        Vector(half_frame_size, half_frame_size, z), Rotation())
+    x = frame.Shape.BoundBox.Center.x
+    y = frame.Shape.BoundBox.Center.y
+    z = frame.Shape.BoundBox.ZMin
+    placement = Placement(Vector(x, y, z), Rotation())
     origin_translation_offset = Vector(0.5, 0.5, 0)
     return {
         'placement': placement,
