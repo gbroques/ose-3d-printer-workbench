b162c8792a284abc8dcd898e2b53aded56677a86
G Roques <groques360@gmail.com>
2020-05-11 03:44:36 -0500

    Update docstring

--- ose3dprinter/app/__init__.py
+++ ose3dprinter/app/__init__.py
@@ -1,5 +1,5 @@
 """
-The core package is independent of the workbench package,
+The app package is independent of the gui package,
 must not know about the FreeCAD GUI,
 and is able to be tested from a FreeCAD command-line environment.
 """
