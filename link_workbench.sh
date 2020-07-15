e84b048f8685f0af5bb246ae91db3c1ce7469289
G Roques <groques360@gmail.com>
2020-07-15 06:54:15 -0500

    Update and fix link_workbench.sh

--- link_workbench.sh
+++ link_workbench.sh
@@ -3,20 +3,18 @@
 # Create a symbolic link of workbench to FreeCAD Mod directory. #
 # ------------------------------------------------------------- #
 
-set -x  # Print shell command before executing
 set -e  # Exit shell script upon non-zero status code (error) of command
 
-# Create temporary python script to find user's FreeCAD Mod directory
-touch find_mod_dir.py
-echo "import FreeCAD" >> find_mod_dir.py
-echo "print App.getUserAppDataDir() + 'Mod'" >> find_mod_dir.py
-echo "exit(0)" >> find_mod_dir.py
+if ! [ -x "$(command -v freecad)" ]; then
+    echo "freecad command not found."
+    exit
+fi
 
-# TODO: Check if freecad command is available before executing
-mod_dir=`freecad -c find_mod_dir.py | tail -1`
-
-# Clean-up temporary python script and .pyc file
-rm find_mod_dir.py*
+app_data_dir=$(freecad -c "import FreeCAD; print(FreeCAD.getUserAppDataDir())")
+mod_dir="${app_data_dir}Mod"
+repo_name=$(basename $(pwd))
 
 # Soft link workbench repository to user's FreeCAD Mod directory
-ln -s $(pwd) $mod_dir/ose-3d-printer-workbench
+echo "Linking workbench to FreeCAD Mod directory with:\n"
+echo "    ln -s $(pwd) $mod_dir/$repo_name\n"
+ln -s $(pwd) $mod_dir/$repo_name
